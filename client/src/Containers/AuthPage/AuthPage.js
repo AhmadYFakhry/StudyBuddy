f2000e4bf20e512c2f203de70d4e698e40db486f
Ahmad Fakhry <ahmad.fakhry@mail.utoronto.ca>
2020-05-06 02:47:48 -0400

    Login page now uses hooks

--- client/src/Containers/AuthPage/AuthPage.js
+++ client/src/Containers/AuthPage/AuthPage.js
@@ -1,62 +1,52 @@
-import React from 'react'
-import './AuthPage.css'
+import React, {useState} from 'react'
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { Container, Alert } from "react-bootstrap";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { Container } from "react-bootstrap";
 import LoginForm from '../../Components/Login/Login.js'
+import './AuthPage.css'
 
+export default function AuthPage(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [open, setOpen] = useState(false)
 
-class AuthPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      email: "",
-      password: "",
-      alert: <div></div>,
-      showSpinner: false
-    }
-    this.handleLogin = this.handleLogin.bind(this);
-    this.updatePassword = this.updatePassword.bind(this);
-    this.updateEmail = this.updateEmail.bind(this);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') return;
+    setOpen(false);
   }
 
-  updatePassword(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  updateEmail(e) {
-    this.setState({ email: e.target.value });
-
-  }
-
-  async handleLogin(e) {
+  const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post("http://localhost:8000/user/login", {
-        email: this.state.email,
-        password: this.state.password
+        email,
+        password
       })
       Cookies.set('uid', res.data.user._id);
       Cookies.set('Authorization', 'Bearer ' + res.data.tk, { path: '' });
-      window.location.href = "/dashboard"
+      props.history.push("/dashboard")
     } catch (error) {
-      console.log(error);
-      this.setState({ alert: <Alert variant="warning">Unable to login! Could not find a matching email and password</Alert> })
+      setOpen(true)
     }
   }
-  render() {
-    return (
-      < Container className="container" >
-        <LoginForm
-          loginHandler={this.handleLogin}
-          updateEmail={this.updateEmail}
-          updatePassword={this.updatePassword}
-          spinnerHandler={this.spinnerHandler}>
-        </LoginForm>
-        {this.state.alert}
-      </Container >
-    )
-  }
+  return (
+    <Container className="container" >
+      <Snackbar  
+          open={open} 
+          autoHideDuration={2500} 
+          onClose={handleClose}
+          anchorOrigin={{vertical: 'top', horizontal: 'right'}}>
+          <MuiAlert variant="filled" onClose={handleClose} severity="error">
+              Could not find a valid account with that email and password combination. Please register if you haven't
+          </MuiAlert>
+      </Snackbar>
+      <LoginForm
+        loginHandler={handleLogin}
+        updateEmail={e => setEmail(e.target.value)}
+        updatePassword={e => setPassword(e.target.value)}>
+      </LoginForm>
+    </Container >
+  )
 }
-
-export default AuthPage;
